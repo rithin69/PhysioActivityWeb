f9866f5b0a15f3257e1034945ce68fc4bc7dc182
rithin69 <69008632+rithin69@users.noreply.github.com>
2024-11-12 15:26:26 +0530

    add button

--- Frontend/src/Patientdashboard.js
+++ Frontend/src/Patientdashboard.js
@@ -34,11 +34,11 @@ const Dashboard = () => {
     <div className="container mx-auto p-4">
 
       {/* Personal  Information  */}
-      <section className="bg-gray-100 p-6 rounded-lg shadow-lg mb-8">
+      <section className="bg-gray-100 p-6 rounded-lg shadow-lg mb-8 relative ">
         <h2 className="text-2xl font-bold text-gray-900 mb-4">Personal Information</h2>
         {/* Add Notes Button */}
         <button
-          className="absolute top-6 right-8 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md transition duration-200"
+          className="absolute top-6 right-8 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md transition duration-200 absolute"
           onClick={() => setSidePanelOpen(true)} // Open SidePanel on click
         >
           Add Notes
