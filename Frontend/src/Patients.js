5037fbca976e4b3a55e451ba8d055ee0997131d3
rithin69 <69008632+rithin69@users.noreply.github.com>
2025-01-22 23:46:43 +0000

    fixed dashboard layout

--- Frontend/src/Patients.js
+++ Frontend/src/Patients.js
@@ -276,7 +276,7 @@ const Patients = () => {
               <div className="flex space-x-2 mb-4">
                 <button
                   onClick={() => openModal(appointment)}
-                  className="w-full px-4 py-2 bg-yellow-500 text-white rounded-md flex items-center justify-center"
+                  className="w-3/4 px-4 py-2 bg-yellow-500 text-white rounded-md flex items-center justify-center"
                 >
                   <PencilAltIcon className="w-5 h-5 mr-1" />
                   Add Notes
