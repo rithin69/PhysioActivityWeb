4044684809a90630155b1cecf6a36522a7e2c381
rithin69 <69008632+rithin69@users.noreply.github.com>
2025-01-31 18:18:01 +0000

    fitbit

--- Frontend/src/Navbar.js
+++ Frontend/src/Navbar.js
@@ -72,11 +72,13 @@ const Navbar = ({ isSidebarExpanded }) => {
   
 
   return (
+    
     <div
       className={`sticky top-0 z-50 bg-white flex items-center justify-between px-6 py-4 shadow-md transition-all duration-300 ${
         isSidebarExpanded ? 'ml-64' : 'ml-20'
       }`}
     >
+      {console.log("hhhhhhhhhhhhhhhhh")}
       <div className="flex items-center">{/* Add a brand logo or name */}</div>
 
       {/* Right Section with Icons */}
